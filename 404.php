0421e481bb9c9ff27742e666c90fffeefa73aaa6
Roman <sheikhroman042@gmail.com>
2023-07-03 23:02:58 +0600

    archive.php

---
+++ 404.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404</title>
+</head>
+<body>
+<h1 style="color: red; font-size: 40px; text-align:center; margin-top:90px;">404 <br>  ! ERROR !</h1>
+</body>
+</html>
---
+++ archive.php
@@ -0,0 +1,56 @@
+<?php
+/* this templete for displaying the header */
+
+// use JetBrains\PhpStorm\Language; 
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="<?php language_attributes(); ?>" class="no-js">
+
+<head>
+    <meta charset="<?php bloginfo('charset') ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+    <!-- Header Area -->
+    <header id="header_area" class="<?php echo get_theme_mod('roman_menu_position') ?> ">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <a href="<?php echo home_url(); ?>"><img src=" <?php echo get_theme_mod('roman_logo') ?> " alt=""></a>
+                </div>
+                <div class="col-md-9">
+                    <?php wp_nav_menu(array('theme_location' => 'main_menu', 'menu_id' => 'nav')); ?>
+                </div>
+            </div>
+        </div>
+    </header>
+<h1>ARCHIVE</h1>
+    <!-- Body area -->
+    <section id="body_area">
+        <div class="container">
+            <div class="row col-md-12">
+                <?php the_content(); ?>
+            </div>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <footer id="footer_area">
+        <section id="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <p><?php echo get_theme_mod('roman_copyright_section'); ?></p>
+                </div>
+            </div>
+        </section>
+    </footer>
+
+    <?php wp_footer(); ?>
+</body>
+
+</html>

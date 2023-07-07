9326c40dc1ba54e71ba415f3d892289036f8fcea
Roman <sheikhroman042@gmail.com>
2023-07-07 18:02:57 +0600

    notif

--- archive.php
+++ archive.php
@@ -1,34 +1,7 @@
 <?php
-/* this templete for displaying the header */
 
-// use JetBrains\PhpStorm\Language; 
-
-?>
-
-
-<!DOCTYPE html>
-<html lang="<?php language_attributes(); ?>" class="no-js">
-
-<head>
-    <meta charset="<?php bloginfo('charset') ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php wp_head(); ?>
-</head>
-
-<body <?php body_class(); ?>>
     <!-- Header Area -->
-    <header id="header_area" class="<?php echo get_theme_mod('roman_menu_position') ?> ">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-3">
-                    <a href="<?php echo home_url(); ?>"><img src=" <?php echo get_theme_mod('roman_logo') ?> " alt=""></a>
-                </div>
-                <div class="col-md-9">
-                    <?php wp_nav_menu(array('theme_location' => 'main_menu', 'menu_id' => 'nav')); ?>
-                </div>
-            </div>
-        </div>
-    </header>
+    get_header();?>
 <h1>ARCHIVE PAGE</h1>
     <!-- Body area -->
     <section id="body_area">
@@ -54,3 +27,4 @@
 </body>
 
 </html>
+?>
--- index.php
+++ index.php
@@ -1,43 +1,6 @@
 <?php
-/* this templete for displaying the header */
 
-// use JetBrains\PhpStorm\Language; 
-
-?>
-
-
-<!DOCTYPE html>
-<html lang="<?php language_attributes(); ?>" class="no-js">
-
-<head>
-    <meta charset="<?php bloginfo('charset') ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php wp_head(); ?>
-</head>
-
-<body <?php body_class(); ?>>
-    <!-- Header Area -->
-    <header id="header_area" class="<?php echo get_theme_mod('roman_menu_position') ?> ">
-    <div id="notice">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <p>Some Notification</p>
-                </div>
-            </div>
-        </div>
-    </div>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-3">
-                    <a href="<?php echo home_url(); ?>"><img src=" <?php echo get_theme_mod('roman_logo') ?> " alt=""></a>
-                </div>
-                <div class="col-md-9">
-                    <?php wp_nav_menu(array('theme_location' => 'main_menu', 'menu_id' => 'nav')); ?>
-                </div>
-            </div>
-        </div>
-    </header>
+get_header();?>
 
     <!-- Body area -->
     <section id="body_area">

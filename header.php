e2959474a532290ea3dcb065e3749cd24e34e319
Roman <sheikhroman042@gmail.com>
2023-07-28 16:22:00 +0600

    post showing in real

--- header.php
+++ header.php
@@ -18,15 +18,6 @@
 <body <?php body_class(); ?>>
     <!-- Header Area -->
     <header id="header_area" class="<?php echo get_theme_mod('roman_menu_position') ?> ">
-    <div id="notice">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <p>Some Notification</p>
-                </div>
-            </div>
-        </div>
-    </div>
         <div class="container">
             <div class="row">
                 <div class="col-md-3">
--- index.php
+++ index.php
@@ -7,8 +7,22 @@ get_header(); ?>
   <section id="body_area">
     <div class="container">
       <div class="row">
-        <div class="col-md-12">
+        <div class="col-md-9">
+        <?php if(have_posts(  )):
+            while(have_posts(  )):the_post(  );
+             ?>
+          <div class="blog_area">
           <?php the_content(); ?>
+          </div>
+          <?php 
+          endwhile;
+        else:
+          _e('No post found');
+        endif;
+          ?>
+        </div>
+        <div class="col-md-3">
+          <h2>Side bar area</h2>
         </div>
       </div>
     </div>

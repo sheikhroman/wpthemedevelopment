201d2b5e8691bcdeb85b39525316fa8970a10716
Roman <sheikhroman042@gmail.com>
2023-08-03 12:21:07 +0600

    read more btn

--- inc/default.php
+++ inc/default.php
@@ -8,4 +8,14 @@ add_theme_support( 'post-thumbnails',array('post','page') );
 add_image_size('post-thumbnails', 970, 350,true);
 
 
+//except to 40 words
+function roman_excerpt_more($more){
+    return '<a href="'.get_permalink($post->ID).'">'.'Read More'.'</a>';
+}
 
+add_filter( 'excerpt_more', 'roman_excerpt_more');
+
+function roman_excerpt_length($length){
+    return 40;
+}
+add_filter( 'excerpt_length', 'roman_excerpt_length' ,999);

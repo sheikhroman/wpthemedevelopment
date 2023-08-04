dac9fc056f39cb305fec893b46e2908681d3280f
Roman <sheikhroman042@gmail.com>
2023-08-04 12:00:49 +0600

    readmore close

--- inc/default.php
+++ inc/default.php
@@ -1,21 +1,21 @@
 <?php
-//theme title
 
-add_theme_support( 'title-tag' );
+// Theme Title
+add_theme_support('title-tag');
 
-//thumble image
-add_theme_support( 'post-thumbnails',array('post','page') );
-add_image_size('post-thumbnails', 970, 350,true);
+// Thumbnil Image Area
+add_theme_support( 'post-thumbnails', array('page', 'post') );
+add_image_size('post-thumbnails', 970, 350, true);
 
 
-//except to 40 words
-function roman_excerpt_more($more){
-    return '<a href="'.get_permalink($post->ID).'">'.'Read More'.'</a>';
-}
+// Except to 40 Word
 
-add_filter( 'excerpt_more', 'roman_excerpt_more');
+// function roman_excerpt_more($more){
+//   return '<a class="redmore" href="'.get_permalink( $post->ID) . '">' . ' Read More' . '</a>';
+// }
+// add_filter('excerpt_more', 'roman_excerpt_more');
 
-function roman_excerpt_length($length){
-    return 40;
-}
-add_filter( 'excerpt_length', 'roman_excerpt_length' ,999);
+// function roman_excerpt_lenght($length){
+//   return 40;
+// // }
+// add_filter('excerpt_length', 'roman_excerpt_lenght', 999);

e73492c2fc24d3844f5ed9e67d9f42fb13544f6c
Roman <sheikhroman042@gmail.com>
2023-08-19 15:42:12 +0600

    doing 404

---
+++ 404.php
@@ -0,0 +1,20 @@
+<?php
+/*
+* The main template file not found
+*/ 
+get_header(); ?>
+
+  <section id="body_area">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 error_page">
+            <h2>404</h2>
+            <p>Sorry, but the page you are looking for does not exist.</p>
+            <?php get_search_form(); ?>
+        </div>
+        <a href="<?php echo home_url(); ?>" class="homepage">Home</a>
+      </div>
+    </div>
+  </section>
+
+  <?php get_footer(); ?>
--- index.php
+++ index.php
@@ -9,6 +9,10 @@ get_header(); ?>
       <div class="row">
         <div class="col-md-9">
         <?php get_template_part('template_part/blog_setup') ?>
+        </div>
+        <div class="col-md-3">
+          <?php get_sidebar(); ?>
+        </div>
     </div>
   </section>
 

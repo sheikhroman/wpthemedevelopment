4a4dd4fedc9ff2a74ae5d497ae4594e2d39b7eb7
Roman <sheikhroman042@gmail.com>
2023-09-13 12:30:27 +0600

    error

---
+++ font-page.php
@@ -0,0 +1,26 @@
+<?php
+// theme font
+get_header();?>
+<section id="service_area">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h2>This is title</h2>
+                <img src="img/1.jpg" alt="">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, maiores asperiores iste placeat quam esse aliquam hic repudiandae. </p>
+            </div>
+            <div class="col-md-4">
+                <h2>This is title</h2>
+                <img src="img/1.jpg" alt="">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, maiores asperiores iste placeat quam esse aliquam hic repudiandae. </p>
+            </div>
+            <div class="col-md-4">
+                <h2>This is title</h2>
+                <img src="img/1.jpg" alt="">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, maiores asperiores iste placeat quam esse aliquam hic repudiandae. </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer();?>

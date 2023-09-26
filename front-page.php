0da8384ebdcc6c13fc1c4b62e4174f096606c5e1
Roman <sheikhroman042@gmail.com>
2023-09-26 12:07:09 +0600

    fornt page done

--- front-page.php
+++ front-page.php
@@ -1,26 +1,27 @@
 <?php
 // theme font
-get_header();?>
+get_header(); ?>
 <section id="service_area">
     <div class="container">
         <div class="row">
-            <div class="col-md-4">
-                <h2>This is title</h2>
-                <img src="img/1.jpg" alt="">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, maiores asperiores iste placeat quam esse aliquam hic repudiandae. </p>
-            </div>
-            <div class="col-md-4">
-                <h2>This is title</h2>
-                <img src="img/1.jpg" alt="">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, maiores asperiores iste placeat quam esse aliquam hic repudiandae. </p>
-            </div>
-            <div class="col-md-4">
-                <h2>This is title</h2>
-                <img src="img/1.jpg" alt="">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, maiores asperiores iste placeat quam esse aliquam hic repudiandae. </p>
-            </div>
+            <?php
+            query_posts('post_type=service&post_status=publish&posts_per_page=3&order=ASC&paged=' . get_query_var('post'));
+            if (have_posts()) :
+                while (have_posts()) : the_post();
+            ?>
+                    <div class="col-md-4">
+                        <div class="child_service">
+                            <h2><?php the_title(); ?></h2>
+                            <?php echo the_post_thumbnail('service') ?>
+                            <p><?php the_excerpt() ?> </p>
+                        </div>
+                    </div>
+            <?php
+                endwhile;
+            endif;
+            ?>
         </div>
     </div>
 </section>
 
-<?php get_footer();?>
+<?php get_footer(); ?>

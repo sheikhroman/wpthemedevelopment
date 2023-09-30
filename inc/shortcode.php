03dce78ca5798e747f01a9a7c7b6f6e712ea1378
Roman <sheikhroman042@gmail.com>
2023-09-30 12:24:52 +0600

    advance shortcode done

--- inc/shortcode.php
+++ inc/shortcode.php
@@ -12,3 +12,38 @@ function button_shortcode($atts, $content = null){
     return '<a class="button" href="' .esc_attr( $values['url']).'">' .$content.'</a>';
 }
 add_shortcode( 'button', 'button_shortcode' ); // Add a semicolon here
+
+//shortcode & custom post 
+function service_shortcode($atts){
+    ob_start();
+    //query_posts('post_type=service&post_status=publish&posts_per_page=3&order=ASC&paged=' . get_query_var('post'));
+    $query = new WP_Query(array(
+        'post_type' => 'service',
+        'post_per_page' => 3,
+        'order' => 'ASC',
+        'orderby' => 'title',        
+    ));
+    if($query -> have_posts()){?>
+    get_header(); ?>
+<section id="service_area">
+    <div class="container">
+        <div class="row">
+            <?php while($query -> have_posts()) : $query -> the_post();?>
+                    <div class="col-md-4">
+                        <div class="child_service">
+                            <h2><?php the_title(); ?></h2>
+                            <?php echo the_post_thumbnail('service') ?>
+                            <p><?php the_excerpt() ?> </p>
+                        </div>
+                    </div>
+            <?php
+                endwhile; wp_reset_postdata();
+            ?>
+        </div>
+    </div>
+</section>
+    <?php $myvariable = ob_get_clean();
+    return $myvariable;
+    }
+}
+add_shortcode( 'service', 'service_shortcode' );
